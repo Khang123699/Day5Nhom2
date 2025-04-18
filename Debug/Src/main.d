48bcbe616392485799f6e23b3fdc287c434813a2
Khang123699 <khangyeu141@gmail.com>
2025-04-18 14:44:39 +0700

    1

---
+++ Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h
+
+C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+
+C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+
+C:/Users/KHANG/Desktop/STM/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
